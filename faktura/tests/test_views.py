0eef1b2c2097a414740f6e6ecb0c429729c9bd26
Juraj M. Bezrucka <mimmon@gmail.com>
2019-06-17 23:21:09 +0200

    made redirect url universal

--- faktura/tests/test_views.py
+++ faktura/tests/test_views.py
@@ -1,12 +1,15 @@
 import pytest
 
+from django.conf import settings
 from django.urls import reverse
+
+LOGIN_URL = getattr(settings, 'LOGIN_URL', '@nonsense@')
 
 
 @pytest.mark.django_db
 def test_invoice_list_authentication(client, django_user_model):
     response = client.get(reverse('list'))
-    assert response.url.startswith('/login/') is True
+    assert response.url.startswith(LOGIN_URL)
     assert response.status_code == 302
 
     user = django_user_model.objects.create_user(
